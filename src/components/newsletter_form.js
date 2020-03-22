85f5aa2dc49cf0ca3f868e16f6a341ebaf0a85d6
Reno DuBois <renodubois@users.noreply.github.com>
2020-03-22 16:53:16 -0500

    Add newsletter form functionality

--- src/components/newsletter_form.js
+++ src/components/newsletter_form.js
@@ -1,9 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
 import "./newsletter.css"
 
 const NewsletterForm = () => {
+  const [formResponse, setFormResponse] = useState({
+    success: false,
+    message: "",
+  })
   return (
-    <form>
+    <form
+      onSubmit={async event => {
+        event.preventDefault()
+        const response = await fetch("/newsletter/signup", {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify({ email: event.target.elements["email"].value }),
+        })
+        const body = await response.json()
+        if (body.success) {
+          setFormResponse({
+            success: true,
+            message: "You have been subscribed to the newsletter!",
+          })
+        } else {
+          console.error(body.error)
+          setFormResponse({
+            success: false,
+            message: "Oops, something went wrong! Please try again.",
+          })
+        }
+      }}
+    >
+      {formResponse.message ? (
+        <div
+          style={{
+            backgroundColor: formResponse.success ? "green" : "red",
+            padding: 15,
+            borderRadius: 5,
+            marginBottom: 15,
+            color: "white",
+            fontWeight: "bold",
+          }}
+        >
+          {formResponse.message}
+          <button
+            style={{ float: "right", padding: 0, color: "white" }}
+            onClick={() => setFormResponse({ ...formResponse, message: "" })}
+          >
+            x
+          </button>
+        </div>
+      ) : null}
       <input
         type="email"
         name="email"
